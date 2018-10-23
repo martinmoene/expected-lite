f7d5f1797a075b51686534c56f8b66e93fbcd7f5
Martin Moene <martin.moene@gmail.com>
2018-10-23 23:53:47 +0200

    Release 0.2.0

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile
 
 class ExpectedLiteConan(ConanFile):
-    version = "0.0.0"
+    version = "0.2.0"
     name = "expected-lite"
     description = "Expected objects for C++11 and later"
     license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
