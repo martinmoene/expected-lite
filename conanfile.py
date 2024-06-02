643873ef533886c636fa27c0b5519194ee20d25e
Martin Moene <martin.moene@gmail.com>
2024-06-02 11:23:50 +0200

    Release 0.7.0
    Fixes #57, #58, #60, #61, #63, #65 and nonstd-lite 72
    
    https://github.com/martinmoene/nonstd-lite-project/issues/72

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile, CMake
 
 class ExpectedLiteConan(ConanFile):
-    version = "0.6.3"
+    version = "0.7.0"
     name = "expected-lite"
     description = "Expected objects for C++11 and later"
     license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
